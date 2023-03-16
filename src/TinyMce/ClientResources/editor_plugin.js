b87124eeb3e2e6479cdbdf766cdebfc9cce054f6
Ole <ole.tellefsen@bouvet.no>
2023-03-16 14:54:19 +0100

    wip support for tinymce 5 and newer

--- src/TinyMce/ClientResources/editor_plugin.js
+++ src/TinyMce/ClientResources/editor_plugin.js
@@ -6,48 +6,53 @@ var currentUrl = location.href;
 tinymce.PluginManager.add("getaepiimageshop", function (ed, url) {
     var imageNode;
 
-    // Register commands
-    ed.addCommand('mcegetaepiimageshop', function () {
+    if (tinymce.majorVersion >= 5) {
 
-        try {
-            var baseUrl = location.protocol + '//' + location.host;
-            var dialogUrl = baseUrl + "/imageshoptinymce/insertimage/?tinyMce=True";
-            console.log("Url for dialog: " + dialogUrl);
+    }
+    else {
+        // Register commands
+        ed.addCommand('mcegetaepiimageshop', function () {
 
-            if (ed.selection !== null && ed.selection.getNode() !== null && ed.selection.getNode().src !== null)
-                dialogUrl += "&image=" + encodeURIComponent(ed.selection.getNode().src);
+            try {
+                var baseUrl = location.protocol + '//' + location.host;
+                var dialogUrl = baseUrl + "/imageshoptinymce/insertimage/?tinyMce=True";
+                console.log("Url for dialog: " + dialogUrl);
 
-            ed.windowManager.open({
-                file: dialogUrl,
-                width: 950,
-                height: 650,
-                scrollbars: 1,
-                inline: 1
-            }, {
-                plugin_url: url
-            });
-        } catch (error) {
-            console.log("Error running command mcegetaepiimageshop: " + error);
-        }
-    });
+                if (ed.selection !== null && ed.selection.getNode() !== null && ed.selection.getNode().src !== null)
+                    dialogUrl += "&image=" + encodeURIComponent(ed.selection.getNode().src);
 
-    // Register buttons
-    ed.addButton("getaepiimageshop", {
-        title: "Insert/Upload Imageshop Image",
-        cmd: "mcegetaepiimageshop",
-        image: url + "/images/icon.gif",
-        onPostRender: function () {
-            // Add a node change handler, selects the button in the UI when a image is selected
-            ed.on("NodeChange", function (e) {
-                //Prevent tool from being activated as objects represented as images.
-                var isWebShopImage = e.element.tagName === "IMG" && (ed.dom.getAttrib(e.element, "class").indexOf("ScrImageshopImage") > -1 || ed.dom.getAttrib(e.element, "class").indexOf("mceItem") === -1);
-                this.active(isWebShopImage);
+                ed.windowManager.open({
+                    file: dialogUrl,
+                    width: 950,
+                    height: 650,
+                    scrollbars: 1,
+                    inline: 1
+                }, {
+                    plugin_url: url
+                });
+            } catch (error) {
+                console.log("Error running command mcegetaepiimageshop: " + error);
+            }
+        });
 
-                //Set or reset imageNode to the node cause Image Editor command enabled
-                imageNode = isWebShopImage ? e.element : null;
-            }.bind(this));
-        }
-    });
+        // Register buttons
+        ed.addButton("getaepiimageshop", {
+            title: "Insert/Upload Imageshop Image",
+            cmd: "mcegetaepiimageshop",
+            image: url + "/images/icon.gif",
+            onPostRender: function () {
+                // Add a node change handler, selects the button in the UI when a image is selected
+                ed.on("NodeChange", function (e) {
+                    //Prevent tool from being activated as objects represented as images.
+                    var isWebShopImage = e.element.tagName === "IMG" && (ed.dom.getAttrib(e.element, "class").indexOf("ScrImageshopImage") > -1 || ed.dom.getAttrib(e.element, "class").indexOf("mceItem") === -1);
+                    this.active(isWebShopImage);
+
+                    //Set or reset imageNode to the node cause Image Editor command enabled
+                    imageNode = isWebShopImage ? e.element : null;
+                }.bind(this));
+            }
+        });
+    }
 
     return {
         getMetadata: function () {
